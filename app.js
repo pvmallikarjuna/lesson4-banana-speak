ec0c408c2e38e1e556dc7daa572821c14340f990
pvmallikarjuna <pvmallikarjuna.dev@gmail.com>
2022-09-02 01:15:46 +0530

    Updated Style and Working Code

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+//Get DOM elements of button, input and output
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", clickHandler)
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function clickHandler(){
-
-    outputDiv.innerText = "translated blabla: "+txtInput.value; 
-   
+//Take paramter and generate URL with parameter
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error) {
+  console.log("Error has occured : ", error);
+  alert("A Server error has occured");
+}
 
+btnTranslate.addEventListener("click", clickHandler);
 
+//Call Function when Translate button is clicked    
+function clickHandler() {
+  // outputDiv.innerText = "translated blabla: "+txtInput.value;
 
+  var inputTxt = txtInput.value;
+
+  //Calling Server for processing
+  fetch(getTranslationURL(inputTxt))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText; //Output
+    })
+    .catch(errorHandler);
+}
